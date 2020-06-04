74ec6d305b8a429c2e412903aa95cd21a3d7fd74
ecemarc <65424986+ecemarc@users.noreply.github.com>
2020-06-04 18:43:28 -0400

    6.4.20 update with Deli Name

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,79 +1,95 @@
 # shopping_cart.py
 
+import datetime
 products = [
-    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
-    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
-    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
-    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
-    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
-    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
-    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
-    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
-    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
-    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
-    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
-    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
-    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
-    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
-    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
-    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
-    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
-    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
-    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
-    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+    {"id": 1, "name": "Chocolate Sandwich Cookies",
+        "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
+    {"id": 2, "name": "All-Seasons Salt", "department": "pantry",
+        "aisle": "spices seasonings", "price": 4.99},
+    {"id": 3, "name": "Robust Golden Unsweetened Oolong Tea",
+        "department": "beverages", "aisle": "tea", "price": 2.49},
+    {"id": 4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce",
+        "department": "frozen", "aisle": "frozen meals", "price": 6.99},
+    {"id": 5, "name": "Green Chile Anytime Sauce", "department": "pantry",
+        "aisle": "marinades meat preparation", "price": 7.99},
+    {"id": 6, "name": "Dry Nose Oil", "department": "personal care",
+        "aisle": "cold flu allergy", "price": 21.99},
+    {"id": 7, "name": "Pure Coconut Water With Orange",
+        "department": "beverages", "aisle": "juice nectars", "price": 3.50},
+    {"id": 8, "name": "Cut Russet Potatoes Steam N' Mash",
+        "department": "frozen", "aisle": "frozen produce", "price": 4.25},
+    {"id": 9, "name": "Light Strawberry Blueberry Yogurt",
+        "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
+    {"id": 10, "name": "Sparkling Orange Juice & Prickly Pear Beverage",
+        "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
+    {"id": 11, "name": "Peach Mango Juice", "department": "beverages",
+        "aisle": "refrigerated", "price": 1.99},
+    {"id": 12, "name": "Chocolate Fudge Layer Cake",
+        "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
+    {"id": 13, "name": "Saline Nasal Mist", "department": "personal care",
+        "aisle": "cold flu allergy", "price": 16.00},
+    {"id": 14, "name": "Fresh Scent Dishwasher Cleaner",
+        "department": "household", "aisle": "dish detergents", "price": 4.99},
+    {"id": 15, "name": "Overnight Diapers Size 6",
+        "department": "babies", "aisle": "diapers wipes", "price": 25.50},
+    {"id": 16, "name": "Mint Chocolate Flavored Syrup",
+        "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
+    {"id": 17, "name": "Rendered Duck Fat", "department": "meat seafood",
+        "aisle": "poultry counter", "price": 9.99},
+    {"id": 18, "name": "Pizza for One Suprema Frozen Pizza",
+        "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
+    {"id": 19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend",
+        "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
+    {"id": 20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink",
+        "department": "beverages", "aisle": "juice nectars", "price": 4.25}
+]  # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-def to_usd(my_price):
-    """
-    Converts a numeric value to usd-formatted string, for printing and display purposes.
+# def to_usd(my_price):
+#     """
+#     Converts a numeric value to usd-formatted string, for printing and display purposes.
 
-    Param: my_price (int or float) like 4000.444444
+#     Param: my_price (int or float) like 4000.444444
 
-    Example: to_usd(4000.444444)
+#     Example: to_usd(4000.444444)
 
-    Returns: $4,000.44
-    """
-    return f"${my_price:,.2f}" #> $12,000.71
+#     Returns: $4,000.44
+#     """
+#     return f"${my_price:,.2f}" #> $12,000.71
 
 # TODO: write some Python code here to produce the desired output
 
 # print(products)
 
-#
-#INFORMATION COLLECT
-#
-
 total_price = 0
-selected_ids = []
+selected_product = []
 
-#seperating the processes, first store all items in a list than perform the others
 while True:
-    selected_id = input("Please insert a product indifier:")  #string version of the inputl
-    if selected_id=="DONE":     #this has to be here so if can continue if not done.
+    selected_id = input("Please enter your product: ")
+    if selected_id == "DONE" or selected_id == "done":
         break
     else:
-        # matching_products = [p for p in products if str(p["id"]) == str(selected_id)] #use selected id to identify the item
-        # matching_product = matching_products [0] # get the first item
-        # total_price = total_price + matching_product["price"]
-        # print("SELECTED PRODUCT: "+ matching_product ["name"] + " " + str(matching_product ["price"]))
-        selected_ids.append(selected_id)
+        selected_product.append(selected_id)
+
+now = datetime.datetime.now()
 
 
+print("            CORONA-FREE DELI            ")
+print("    888 Hope Avenue, Manhattan, NY 88888 ")
 
-    
+print("Current date and time: ")
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
 
-# print(type(matching_product))
 
-# print(type(selected_id))
-#
-#DISPLAY
-#
-# print(selected_ids["name"])
+for selected_id in selected_product:
+    matching_ids = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_id = matching_ids[0]
+    total_price = total_price + matching_id["price"]
+    print("Selected Product: " +
+          matching_id["name"] + " " + "$" + str(matching_id["price"]))
 
-for selected_id in selected_ids:
-    matching_products = [p for p in products if str(p["id"]) == str(selected_id)] #use selected id to identify the item
-    matching_product = matching_products [0] # get the first item
-    total_price = total_price + matching_product["price"]
-    print("SELECTED PRODUCT: "+ matching_product ["name"] + " " + str(matching_product ["price"]))
 
-print("TOTAL PRICE: " + str(total_price)) #convert the total_price to string to concatinate.\
+def to_usd(total_price):
+    return f"${total_price:,.2f}"  # > $12,000.71
+
+
+print("Total Price" + "              " + to_usd(total_price))
